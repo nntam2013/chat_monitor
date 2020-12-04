0d6358e26b385719f0a53e70e6ad37d73e158117
Tam Nguyen <nntam2013@gmail.com>
2020-12-04 17:26:15 +0700

    add webhook

--- src/config/express.ts
+++ src/config/express.ts
@@ -1,5 +1,6 @@
 import * as bodyParser from 'body-parser'
 import express, { Request, Response } from 'express'
+import mysql from 'mysql'
 
 const app = express()
 
@@ -9,11 +10,68 @@ app.use(bodyParser.json())
 app.get('/', (req, res) => {
   res.json({ ok: true })
 })
+type EventType = 'message.new' | 'channel.updated' | 'channel.created'
 
+const getRole = (user: any) =>
+  user?.company?.id === 1
+    ? 'operation'
+    : user?.id?.includes('employer')
+    ? 'employer'
+    : 'freelancer'
 const handleChatEvent = (request: Request, response: Response) => {
-  console.log('------------POST-------')
-  console.log(request.body)
   const data = request.body
+  const type = data?.type as EventType
+
+  const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+  })
+
+  if (type === 'channel.created' || type === 'channel.updated') {
+    // Add data to channel_event table
+    const channel = data?.channel
+    const saveData = {
+      type,
+      channel_id: data?.channel_id,
+      company_id: channel?.companyId,
+      region_id: channel?.region,
+      created_at: new Date(channel?.created_at),
+    }
+    connection.query(
+      'INSERT INTO channel_events SET ?',
+      saveData,
+      (error: any, results: any, fields: any) => {
+        if (error) throw error
+        console.log(results.insertId)
+      },
+    )
+    return response.json({ ok: true })
+  }
+  if (type === 'message.new') {
+    // Add data to message_event table
+    const user = data?.user
+    const message = data?.message
+    const saveData = {
+      id: message?.id,
+      user_id: user?.id,
+      channel_id: data?.channel_id,
+      company_id: user?.company?.id,
+      user_role: getRole(user),
+      created_at: new Date(),
+    }
+
+    connection.query(
+      'INSERT INTO message_events SET ?',
+      saveData,
+      (error: any, results: any, fields: any) => {
+        if (error) throw error
+        console.log(results.insertId)
+      },
+    )
+  }
+
   response.json({ ok: true })
 }
 app.post('/hooks/chat', handleChatEvent)
--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,23 @@
 import 'reflect-metadata'
 import { createConnection } from 'typeorm'
-
+import mysql from 'mysql'
 import logger from './config/logger'
 import app from './config/express'
 const PORT = process.env.PORT || 5000
-app.listen(PORT, () => {
-  logger.info(`Server running at ${PORT}`)
+
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 })
-// createConnection()
-//   .then(() => {
-//     logger.info('database connection created');
-//     app.listen(PORT, () => {
-//       logger.info(`Server running at ${PORT}`);
-//     });
-//   })
-//   .catch((error: Error) => {
-//     logger.info(`Database connection failed with error ${error}`);
-//   });
+
+connection.connect()
+
+app
+  .listen(PORT, () => {
+    logger.info(`Server running at ${PORT}`)
+  })
+  .on('close', () => {
+    connection.end()
+  })
